dd3d4a8d6279718b71da082eeef402085361dd66
RenatKramarovskyi <vt222_krr@student.ztu.edu.ua>
2025-03-31 17:05:29 +0300

    another one commit

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,16 +1,21 @@
 'use client'
 export default function ArticlesPage() {
 
-    [
-        process.env.DB_USER,
-        process.env.NEXT_PUBLIC_DB_PASSWORD,
-    ].map(variable => (
-        console.log(variable)
-    ))
+
+    function printEnvs() {
+        [
+            process.env.DB_USER,
+            process.env.NEXT_PUBLIC_DB_PASSWORD,
+        ].map(variable => (
+            console.log(variable)
+        ))
+    }
+
 
     return (
         <div>
             It works now
+            <button onClick={printEnvs}>PRINT</button>
         </div>
     );
 }
