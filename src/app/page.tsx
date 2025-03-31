891f45239fcd2fbd290d532804054e9996f07bde
RenatKramarovskyi <vt222_krr@student.ztu.edu.ua>
2025-03-31 16:58:55 +0300

    feat: changed logic of printing envs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,24 @@
 
 export default function Home() {
 
-    [
-        process.env.DB_USER,
-        process.env.DB_PORT,
-        process.env.DB_PASSWORD,
-    ].map(variable => (
-        console.log(variable)
-    ))
+
+
+    function printEnvs() {
+        [
+            process.env.DB_USER,
+            process.env.DB_PORT,
+            process.env.DB_PASSWORD,
+        ].map(variable => (
+            console.log(variable)
+        ))
+    }
+
 
   return (
     <div>
         It works now
+
+        <button onClick={printEnvs}>PRINT</button>
     </div>
   );
 }
