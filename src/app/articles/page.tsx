6f9a76f0f1d74b4b47a0a0857fa8dbd64152cef4
RenatKramarovskyi <vt222_krr@student.ztu.edu.ua>
2025-03-31 16:19:48 +0300

    fix: some render fixes

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 export default function ArticlesPage() {
+
+    [
+        process.env.DB_USER,
+        process.env.NEXT_PUBLIC_DB_PASSWORD,
+    ].map(variable => (
+        console.log(variable)
+    ))
+
     return (
         <div>
             It works now
-            {
-                [
-                    process.env.DB_USER,
-                    process.env.NEXT_PUBLIC_DB_PASSWORD,
-                ].map(variable => (
-                    console.log(variable)
-                ))
-            }
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 
 
 export default function Home() {
+
+    [
+        process.env.DB_USER,
+        process.env.DB_PORT,
+        process.env.DB_PASSWORD,
+    ].map(variable => (
+        console.log(variable)
+    ))
+
   return (
     <div>
         It works now
-        {
-            [
-                process.env.DB_USER,
-                process.env.DB_PORT,
-                process.env.DB_PASSWORD,
-            ].map(variable => (
-                console.log(variable)
-            ))
-        }
     </div>
   );
 }
